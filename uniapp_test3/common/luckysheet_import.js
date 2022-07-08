9f8670f180fb8be3f147868daadbbc8b3ae5b00b
雷子涛 <2726121488@qq.com>
2022-07-08 09:49:26 +0800

    first

---
+++ uniapp_test3/common/luckysheet_import.js
@@ -0,0 +1,20 @@
+import 'luckysheet/dist/plugins/css/pluginsCss.css';
+import 'luckysheet/dist/plugins/plugins.css';
+import 'luckysheet/dist/css/luckysheet.css';
+import 'luckysheet/dist/assets/iconfont/iconfont.css';
+
+import 'luckysheet/dist/plugins/js/plugin';
+import './js/jquery.mousewheel.min';
+import './js/jquery-ui.min';
+import './js/jquery.sPage.min';
+import './js/crypto-api.min.js';
+import './js/uuid.min.js';
+import './js/clipboard.min.js';
+import './js/spectrum.min.js';
+import './js/html2canvas.min.js';
+import './js/localforage.min.js';
+import './js/lodash.min.js';
+import './js/jstat.min.js';
+
+import luckysheet from 'luckysheet/dist/luckysheet.umd';
+export default luckysheet;
---
+++ uniapp_test3/pages/index/index.vue
@@ -0,0 +1,72 @@
+<template>
+	<view class="content">
+		<!-- <image class="logo" src="/static/logo.png"></image> -->
+		<div id="luckysheet" style="margin:0px;padding:0px;position:absolute;width:100%;height:100%;left: 0px;top: 0px;">
+			</div>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	import LuckyExcel from 'luckyexcel'
+	import luckysheet from "../../common/luckysheet_import.js"
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		created(){
+			const script= document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = `https://cdn.jsdelivr.net/npm/luckysheet/dist/plugins/js/plugin.js`;
+			document.head.appendChild(script);
+		},
+		mounted(){
+			var options = {
+					container: 'luckysheet', //luckysheet为容器id
+					title: '',
+					lang: 'zh',
+					showinfobar: false,
+					plugins: ['chart']
+			}
+			luckysheet.create(options)
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
